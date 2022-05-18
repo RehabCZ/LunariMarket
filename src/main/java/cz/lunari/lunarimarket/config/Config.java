6a4ddd7c4da52f4541a7bedead9b94e738770098
RehabCZ <filipvacek67@gmail.com>
2022-05-18 10:10:08 +0200

    Reformat config, rename class

--- src/main/java/cz/lunari/lunarimarket/config/Config.java
+++ src/main/java/cz/lunari/lunarimarket/config/Config.java
@@ -4,6 +4,7 @@ import cz.lunari.lunarimarket.LunariMarket;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
+import java.util.List;
 
 public class Config {
 
@@ -13,17 +14,15 @@ public class Config {
     public void initConfig()
     {
         File configFile = new File(instance.getDataFolder(), "config.yml");
-
         if(!configFile.exists()){ instance.getConfig().options().copyDefaults(); }
         instance.saveDefaultConfig();
-
-        if(getConfigString("shopBlock") == null){ setConfigString("shopBlock", "minecraft:barrel");}
     }
 
     /* Basic Config getters */
     public static String getConfigString(String key){ return instance.getConfig().getString(key); }
     public static Integer getConfigNumber(String key){ return instance.getConfig().getInt(key); }
     public static Boolean getConfigBool(String key){ return instance.getConfig().getBoolean(key); }
+    public static List<String> getConfigStringList(String key){ return instance.getConfig().getStringList(key); }
 
     /* Basic Config setters */
     public static void setConfigString(String key, String value){ instance.getConfig().set(key,value); }
